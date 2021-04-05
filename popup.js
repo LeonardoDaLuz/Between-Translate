8f49d97e9d25d66ae2aaf9eecd1dcb0a4d1d2777
Leonardo a Luz <leoluzprog@gmail.com>
2021-04-05 13:51:43 -0300

    traduzindo bonitamente todos os paragrafos

--- popup.js
+++ popup.js
@@ -23,44 +23,35 @@ function traduzir() {
     /*chrome.storage.sync.get("color", ({ color }) => {
       document.body.style.backgroundColor = color;
     });*/
-
-    console.log("foi2");
-
-    let tst = [
-        [
-            ["père", "father", null, null, 1]
-        ],
-        null,
-        "en"
-    ];
-
-    console.log(tst[0][0][0]);
-
     //document.body.style.backgroundColor = "red";
     let style = document.createElement("style");
     document.body.prepend(style);
     style.innerHTML =
         `
-    .origin {
-        position: relative;
-        line-height: 2em;
-    }
-    .trad {
-        position: absolute;
-        top: 1em;
-        color: blue!important;
-    }
-    .trad * {
-        color: blue!important;
-    }
+        .trad {
+            position: absolute;
+            color: blue;
+            top: 1em;
+            font-size: 0.856em;
+            display: block;    
+        }
+        .origin {
+            line-height: 2em;  
+            position: relative;
+        }
+        .trad * {
+            color: blue!important;
+
+        }
     `;
+
     let ps = document.querySelectorAll("p");
     let els = [...ps].map(p => {
-        let el = document.createElement("p");
+        let el = document.createElement("span");
 
         el.classList.add("trad");
         p.classList.add("origin");
-        el.innerHTML = p.innerHTML;
+        el.innerHTML = p.innerHTML.replaceAll('"', "'").replaceAll(/\n/g, "32594");
         p.prepend(el);
         return el;
     })
@@ -69,20 +60,50 @@ function traduzir() {
 
     function traduzirElementos(els) {
         let arr = els.map(el => el.textContent);
-        console.log(JSON.stringify(arr));
+        console.log(arr);
+        let specialChar = "987456"
+        let arrTratado = JSON.stringify(arr).replaceAll('"', specialChar);
+        console.log(arrTratado.replaceAll(specialChar, '"'));
+        console.log("test parse:");
+        console.log(JSON.parse(arrTratado.replaceAll(specialChar, '"')));
 
-        tradTxt(JSON.stringify(arr), data => {
-            for (i = 0; i < arr.length; i++) {
-                var dataParsed= JSON.parse(data[0][0][0]);
-                els[i].innerHTML = dataParsed[i];
+        tradTxt(arrTratado, data => {
+
+            let newData = data.map(item => item[0]).join('');
+
+            console.log("retorno n tratado:");
+            console.log(newData);
+            console.log("retorno tratado:");
+            let retornoTratado = newData.replaceAll(specialChar, '"').replaceAll("32594", "\\n").replaceAll('"."', '","');
+            console.log(retornoTratado);
+            console.log("retorno parse:");
+            let retornoParseado = JSON.parse(retornoTratado);
+            console.log(retornoParseado);
+
+            console.log(els);
+            for (i = 0; i < els.length; i++) {
+                //console.log("s: "+els[i].innerHTML.length+" t: "+retornoParseado[i].length);
+                //let comp= "s: "+els[i].innerHTML.length+" t: "+retornoParseado[i].length;
+                let fontSize = (els[i].innerHTML.length/retornoParseado[i].length)*0.95;
+                fontSize=fontSize>1?1:fontSize;
+                    
+                els[i].style=" font-size: "+fontSize+"em;";
+                els[i].innerHTML = retornoParseado[i];
+
+               // els[i].innerHTML+=comp;
+
+
             }
+
         })
     }
 
     function tradTxt(txt, callback) {
         fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=pt&dt=t&q=' + txt).then(response => response.json())
             .then(data => {
-                console.log(data[0]);
+
+                callback(data[0]);
+
             })
             .catch(error => {
 
